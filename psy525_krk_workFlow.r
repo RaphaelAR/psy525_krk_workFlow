bd7150a7a797a78f1fe42fa528ed031d3017716f
RaphaelAR <rrodriguez@co-impact.co>
2020-02-17 20:33:38 -0500

    initial commit

---
+++ psy525_krk_workFlow.r
@@ -0,0 +1,31 @@
+---
+title: "Psy525_rodriguez_110220_preregistrationTemplate"
+output: html_document
+author: "Raphael A Rodriguez"
+date: "`r Sys.time()`"
+---
+
+![KRK Experiment Flow Chart](/Users/raphaelrodriguez/Desktop/PhD/Spring 2020/PSY525_Rodriguez_04_02_20_flowchart.png)
+The KRK (Kim, Ritter, and Kim) study is a memory study of complex circuit systems which tests the learning curve and decay rate of various study and testing patterns. The experiment work flow can be separate into four phases: recruitment, experiment preparation, the experiment session, and post-experiment.
+
+## Recruitment
+There are posters throughout campus advertising the need for participants, as well as announcements made in some classes, both asks students to call/email the PI. The PI then provides the participants with possible scheduling options. <br>
+<span style="color: red;">The scheduling process should be automated by creating a doodle poll (or similar software) for scheduling options.</span>
+
+## Experiment Preparation
+Before the participant(s) arrive to the lab for testing, the experimenters must begin the two softwares that will be used during the experiment, including adding the participants IDs and experiment version information. Additionally, the experimenter must provide the necessary schematics, drawing sheets, and demographic/consent forms.
+
+## Experiment Session
+<span style="color: red;">The experiment process itself is documented, however, those who are unfamiliar with the experiment or software have a very hard time following along with the document</span> <br>
+The experiment first welcomes the participant(s) as the enter the lab and places them in front of the appropriate monitor. In first part of the experiment (Protocol 1) the participants go through the tutoring systems which teaches details of the circuit schematic.
+![KRK Schematic](/Users/raphaelrodriguez/Desktop/Screen Shot 2020-02-10 at 8.54.10 PM.png)
+Then in Protocal 2 the participants *play* a troubleshooting game during which they must discover and fix the broken components of the circuit.
+![KRK Troubleshooting Simulation](/Users/raphaelrodriguez/Desktop/Screen Shot 2020-02-10 at 8.54.20 PM.png)
+
+Lastly in Protocol 3, the participants answer declarative and recognition memory questions.
+![Recognition Memory Question](/Users/raphaelrodriguez/Desktop/Screen Shot 2020-02-10 at 8.54.54 PM.png)
+
+<span style="color: red;">Running multiple participants at the same time has been a recognized problem for the experimenters. This is not a problem that can be easily automated but certain protocols in the experiment could be adjusted to make a more congruent process for multiple people.</span>
+
+## Post-Experiment
+Immediately after the session ends the experimenter pays the participant and schedules the next session, if applicable. Once the participants leave, the experimenters must save the programs' data accordingly <span style="color: red;">(The data should be automaticaly saved without the experimenters having to manually rename the files and edit parts of the format to prevent human error)</span>. Finally the experimenters place the particpants files into their individual folders and prep for the next sessions.
